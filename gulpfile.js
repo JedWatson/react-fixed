fb46c39bdc206f0c91b7d4ebb9421f9bcd2fabae
Jed Watson <jed.watson@me.com>
2015-04-08 23:56:14 +1000

    Adding gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+    initGulpTasks = require('react-component-gulp-tasks');
+
+var taskConfig = {
+
+	component: {
+		name: 'Fixed'
+	}
+
+};
+
+initGulpTasks(gulp, taskConfig);
